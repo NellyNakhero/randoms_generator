6715c602c2e43aa9217f30e9eb1da4df97928d49
nellynakhero <nelly.nakhero@gmail.com>
2022-09-12 11:50:39 +0300

    adding files

---
+++ src/main/java/tech/dork/randoms_generator/RandomsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package tech.dork.randoms_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomsGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomsGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/dork/randoms_generator/RandomsGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dork.randoms_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomsGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
